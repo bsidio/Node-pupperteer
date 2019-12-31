6b40ddef3bf9dcab1c85967f08931c901fb183f8
bsidio <siddhartha51997@gmail.com>
2019-12-31 23:31:08 +0530

    Added Comments and did some house Keeping

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const puppeteer = require('puppeteer');
 var fs = require('fs');
 let converter = require('json-2-csv');
 
+//Create Empty Json
 let jsonData=[];
+
+//Define Headers Here
 const col=['name' ,`ENGG MATHS - I`,
     'ENGG MATHS Total',
     'APPLIED SCIENCE',
@@ -15,7 +18,7 @@ const col=['name' ,`ENGG MATHS - I`,
     'total',
     'pc']
 
-   
+//Function to Convert Json to CSV and save files
 function exportToCsvFile(headersArray, dataJsonArray, filename) {
 
     converter.json2csvAsync(dataJsonArray, {prependHeader: false}).then(function (csv) {
@@ -60,26 +63,29 @@ function createFileAsync(filename, content) {
     });
 }
 
-
+//The register num in the form required Padding Remove if not necessary
 const zeroPad = (num, places) => String(num).padStart(places, '0')
 
+//Function for scraping
 async function getdata(){
    
     let data=[]
     const browser = await puppeteer.launch({headless:false});
 
+    // LOOP 
     for (let i = 1; i < 60; i++) {
         let num= await zeroPad(i, 3)
+        //Create Dynamic variable to insert into form
         const reg = await '457cs19'+num;
         console.log(reg)
        
         const page = await browser.newPage();
 
     try{
+
         response =await page.goto('http://www.bteresults.net/MyResults.aspx', {waitUntil: 'load'});
-       // const firstResponse = await page.waitForResponse('http://www.bteresults.net/MyResults.aspx');
-        //const finalResponse = await page.waitForResponse(response => response.url() === 'http://www.bteresults.net/MyResults.aspx' && response.status() === 200);
-        console.log(response.status())
+      console.log(response.status())
+      //The Site gives 503 error due to load. I implemented a reload function until 200 status is returned
         while(response.status()===503)
         {
             response=await page.reload()
@@ -93,26 +99,21 @@ async function getdata(){
     
     console.log(page.url());
 
-    // Type our query into the search bar
+    // Find the Form
     await page.focus('#ctl00_PageContents_txtRegNo');
+    //Type the data into the form
     await page.keyboard.type(reg);
 
     // Submit form
     await page.click('#ctl00_PageContents_igbGetResult');
 
-    // Wait for search results page to load
+    // Wait for  results page to load
     const promise=page.waitForSelector('#ctl00_PageContents_gvwResult', {visible: true})
     await promise;
 
-    //let table = document.querySelector("#ctl00_PageContents_gvwResult");
-    
-    //console.log(marks)
-   // let table2=[]
-   //const featureArticle = (await page.$('table tr td #ctl00_PageContents_lblResult')[1]);
-//console.log(featureArticle)
-//let data=[];
-
+   
   const marks= await page.evaluate(() => {
+      //Find the Table
         const tds = Array.from(document.querySelectorAll('#ctl00_PageContents_gvwResult tr td'))
         
         return tds.map(td => td.innerHTML)
@@ -127,8 +128,11 @@ async function getdata(){
 
       console.log('FOUND!', page.url());
 
+      //Other data that i wanted to save
 const element = await page.$("#ctl00_PageContents_lblResult");
 const text = await page.evaluate(element => element.innerText, element);
+
+//Create a Json object
 var obj={
     'name':text,
     'ENGG MATHS - I' : marks[2],
@@ -150,6 +154,8 @@ var obj={
 
   }
 const newdata=await data
+
+//Push the json object
 jsonData.push(obj)
 newdata['name'] = await text
 await page.close();
@@ -159,11 +165,11 @@ console.log(jsonData)
 
     await browser.close()
 
-
+    //Save the Json Array as CSV
     exportToCsvFile(col, jsonData, 'data')
 
 
 };
 
 
-getdata().then()
+getdata()
